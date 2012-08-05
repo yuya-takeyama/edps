fabbf37bfe5bc0ab41f80a78165582c74f0ec832
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-05 13:54:48 +0900

    Fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
  * This file is part of Edps.
  *
  * (c) Igor Wiedler  <igor@wiedler.ch>
- * (c) Yuya Takeyama <sign.of.the.wolf.petagram @gmail.com>
+ * (c) Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
